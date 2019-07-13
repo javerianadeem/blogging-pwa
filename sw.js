86a759111e10e8ce16adde0ee358c683ae867c8a
Javeria <javerianadeem03@gmail.com>
2019-07-13 14:45:36 +0500

    sw.js activated

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
-
+// Installing service workers
 self.addEventListener('install',evt => {
     console.log("service worker has been installed")
 });
+
+// activate event
+self.addEventListener('activate',evt => {
+    console.log('service worker has been activated')
+})
