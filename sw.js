656d6577c3db88bbe74fa8e5e82a27ff129fab8b
Javeria <javerianadeem03@gmail.com>
2019-07-13 16:30:45 +0500

    audits added and fetch event added

--- sw.js
+++ sw.js
@@ -7,3 +7,7 @@ self.addEventListener('install',evt => {
 self.addEventListener('activate',evt => {
     console.log('service worker has been activated')
 })
+// fetch event
+self.addEventListener('fetch',evt => {
+    console.log('fetch event',evt)
+})
