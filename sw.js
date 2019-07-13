eebdbb63d9d28ce0b96a3f1ea6516b144af51774
Javeria <javerianadeem03@gmail.com>
2019-07-13 18:57:56 +0500

    dynamic cache and offline fallback

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = 'site-static-v2'; 
+const staticCacheName = 'site-static-v3'; 
+const dynamicCacheName = 'site-static-v1'; 
 const assets = [
     '/',
     '/index.html',
@@ -10,8 +11,9 @@ const assets = [
     '/images/blog.jpg',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
     'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+    '/pages/fallback.html'
 ];
-
+  
 // Installing service workers
 
 self.addEventListener('install', evt => {
@@ -30,7 +32,7 @@ self.addEventListener('activate',evt => {
         caches.keys().then(keys => {
             // console.log(keys)
             return Promise.all(keys
-                .filter(key => key !== staticCacheName)
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
                 .map (key => caches.delete(key))
             )
         }) //
@@ -40,8 +42,13 @@ self.addEventListener('activate',evt => {
 self.addEventListener('fetch',evt => {
     // console.log('fetch event',evt)
     evt.respondWith(
-        caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request);
-        })
+        caches.match(evt.request).then(cacheRes => { // matches our assets
+            return cacheRes || fetch(evt.request).then(fetchRes => { // if it there return response || fetch request
+              return caches.open (dynamicCache).then(cache => { // take response open up dynamic cache inside cache
+                  cache.put(evt.request.url,fetchRes.clone()) // put new response we get from the page
+                  return fetchRes; 
+              })  
+            })
+        }).catch(()=> caches.match('/pages/fallback.html'))
     );
 });
