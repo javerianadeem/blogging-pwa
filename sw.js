69c9a25eca735d4b917331cefceebdd162c6b7a8
Javeria <javerianadeem03@gmail.com>
2019-07-14 14:25:39 +0500

    conditional fallback and limiting cache size

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v3'; 
-const dynamicCacheName = 'site-static-v1'; 
+const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
     '/',
     '/index.html',
@@ -13,42 +13,60 @@ const assets = [
     'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
     '/pages/fallback.html'
 ];
-  
-// Installing service workers
 
+// cache size limit function
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if (keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        });
+    }); 
+};
+
+// install event
 self.addEventListener('install', evt => {
     //console.log('service worker installed');
     evt.waitUntil(
-      caches.open(staticCacheName).then(cache => {
-        console.log('caching shell assets');
-        cache.addAll(assets);
-      })
-    )
-  });
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+});
+
 // activate event
-self.addEventListener('activate',evt => {
-    // console.log('service worker has been activated')
+self.addEventListener('activate', evt => {
+    //console.log('service worker activated');
     evt.waitUntil(
         caches.keys().then(keys => {
-            // console.log(keys)
+            //console.log(keys);
             return Promise.all(keys
                 .filter(key => key !== staticCacheName && key !== dynamicCacheName)
-                .map (key => caches.delete(key))
-            )
-        }) //
-    )
+                .map(key => caches.delete(key))
+            );
+        })
+    );
 });
+
 // fetch event
-self.addEventListener('fetch',evt => {
-    // console.log('fetch event',evt)
+self.addEventListener('fetch', evt => {
+    //console.log('fetch event', evt);
     evt.respondWith(
-        caches.match(evt.request).then(cacheRes => { // matches our assets
-            return cacheRes || fetch(evt.request).then(fetchRes => { // if it there return response || fetch request
-              return caches.open (dynamicCache).then(cache => { // take response open up dynamic cache inside cache
-                  cache.put(evt.request.url,fetchRes.clone()) // put new response we get from the page
-                  return fetchRes; 
-              })  
-            })
-        }).catch(()=> caches.match('/pages/fallback.html'))
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    // check cached items size
+                    limitCacheSize(dynamicCacheName, 15);
+                    return fetchRes;
+                })
+            });
+        }).catch(() => {
+            if (evt.request.url.indexOf('.html') > -1) {
+                return caches.match('/pages/fallback.html');
+            }
+        })
     );
 });
